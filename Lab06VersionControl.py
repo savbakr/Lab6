231d1dceef91bca439c3bab0220080ebff1acbca
savbakr <savannahbaker@ufl.edu>
2024-10-23 11:18:21 -0400

    Update Lab06VersionControl.py

--- Lab06VersionControl.py
+++ Lab06VersionControl.py
@@ -10,6 +10,9 @@ def encode(password):
     return res
 
 def main():
+    # NEW: Added this line to store encoded password globally in the function
+    encoded_password = ''  
+    
     coder_continue = True
     while coder_continue:
         print('Menu')
@@ -17,15 +20,26 @@ def main():
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
+        
         option = int(input('\nPlease enter an option: '))
+        
         if option == 1:
             decoded_pass = input('Please enter your password to encode: ')
             encoded_password = encode(decoded_pass)
             print('Your password has been encoded and stored!')
             print()
-        if option == 3:
+            
+        # NEW: Added entire elif block for option 2 
+        elif option == 2:  
+            if encoded_password:  
+                pass  # Placeholder for partner's decode implementation 
+            else:  
+                print("Please encode a password first.")  
+                print()  
+                
+        # CHANGED: elif instead of if, and removed break statement 
+        elif option == 3:  
             coder_continue = False
-            break
-
+            
 if __name__ == '__main__':
     main()
